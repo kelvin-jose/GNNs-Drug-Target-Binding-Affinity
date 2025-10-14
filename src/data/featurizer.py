bb26f9eb9ee1a3086b20222316e4bdbe84a41021
Kelvin <kelvinkonoor@gmail.com>
2025-10-14 22:31:51 +0530

    util to convert given atom to feature vector

--- src/data/featurizer.py
+++ src/data/featurizer.py
@@ -1,3 +1,4 @@
+import numpy as np
 from rdkit import Chem
 from utils.logger import setup_logging
 
@@ -20,3 +21,15 @@ def one_hot(x, choices):
     if x in choices:
         out[choices.index(x)] = 1
     return out
+
+def atom_to_feature_vector(atom):
+    symbol = atom.GetSymbol()
+    atom_onehot = one_hot(symbol, COMMON_ATOMS)
+    degree = atom.GetDegree()  # int
+    formal_charge = atom.GetFormalCharge()
+    num_hs = atom.GetTotalNumHs()
+    aromatic = 1 if atom.GetIsAromatic() else 0
+    hybrid = HYBRIDIZATION_MAP.get(atom.GetHybridization(), -1)
+    hybrid_oh = one_hot(hybrid, list(range(len(HYBRIDIZATION_MAP)+1)))  # include -1 as index 0?
+    vec = atom_onehot + [degree, formal_charge, num_hs, aromatic] + hybrid_oh
+    return np.array(vec, dtype=np.float32)
