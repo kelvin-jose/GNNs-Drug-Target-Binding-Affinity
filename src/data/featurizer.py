145fe9046b74e222396cde7d717bbb5445d1885b
Kelvin <kelvinkonoor@gmail.com>
2025-10-14 22:30:07 +0530

    onehot encoding util

--- src/data/featurizer.py
+++ src/data/featurizer.py
@@ -1,3 +1,4 @@
+from rdkit import Chem
 from utils.logger import setup_logging
 
 logger = setup_logging()
@@ -5,3 +6,17 @@ logger = setup_logging()
 # map for common atoms
 COMMON_ATOMS = ["H", "C", "N", "O", "F", "P", "S", "Cl", "Br", "I", "B", "Si", "Se"]
 ATOM_MAP = {a: i for i, a in enumerate(COMMON_ATOMS)}
+
+HYBRIDIZATION_MAP = {
+    Chem.rdchem.HybridizationType.SP: 0,
+    Chem.rdchem.HybridizationType.SP2: 1,
+    Chem.rdchem.HybridizationType.SP3: 2,
+    Chem.rdchem.HybridizationType.SP3D: 3,
+    Chem.rdchem.HybridizationType.SP3D2: 4
+}
+
+def one_hot(x, choices):
+    out = [0] * len(choices)
+    if x in choices:
+        out[choices.index(x)] = 1
+    return out
