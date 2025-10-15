954b2e72d9db2f8432e312912b749f242effc214
Kelvin <kelvinkonoor@gmail.com>
2025-10-15 22:16:53 +0530

    initializing PDBBindLigandDataset class

--- src/data/pyg_dataset.py
+++ src/data/pyg_dataset.py
@@ -1,3 +1,16 @@
+from pathlib import Path
 from utils.logger import setup_logging
+from torch_geometric.data import InMemoryDataset
 
 logger = setup_logging()
+
+class PDBBindLigandDataset(InMemoryDataset):
+    def __init__(self, metadata_csv="data/processed/refined_dataset_metadata.csv", 
+                 root="data/processed/graphs", transform=None, 
+                 pre_transform=None, force_rebuild=False):
+        self.metadata_csv = Path(metadata_csv)
+        self.root = Path(root)
+        self.root.mkdir(parents=True, exist_ok=True)
+        self.cache_file = self.root / "dataset.pt"
+        self.force_rebuild = force_rebuild
+        super().__init__(self.root, transform, pre_transform)
