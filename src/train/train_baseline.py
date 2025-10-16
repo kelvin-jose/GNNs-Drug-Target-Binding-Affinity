c9601e1f7af51a6844734fd24dcb37109db0a380
Kelvin <kelvinkonoor@gmail.com>
2025-10-16 12:17:25 +0530

    simple random split

--- src/train/train_baseline.py
+++ src/train/train_baseline.py
@@ -12,3 +12,14 @@ def set_seed(seed=SEED):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+
+def scaffold_split(dataset, frac_train=0.8, frac_val=0.1, frac_test=0.1, seed=SEED):
+    num = len(dataset)
+    idx = list(range(num))
+    random.Random(seed).shuffle(idx)
+    ntrain = int(frac_train * num)
+    nval = int(frac_val * num)
+    train_idx = idx[:ntrain]
+    val_idx = idx[ntrain:ntrain+nval]
+    test_idx = idx[ntrain+nval:]
+    return train_idx, val_idx, test_idx
