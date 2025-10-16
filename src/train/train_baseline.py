2e527503cb7cfac28261a6477700499f0b7ed54a
Kelvin <kelvinkonoor@gmail.com>
2025-10-16 12:16:25 +0530

    setting seed

--- src/train/train_baseline.py
+++ src/train/train_baseline.py
@@ -1,3 +1,14 @@
+import torch
+import random
+import numpy as np
 from utils.logger import setup_logging
 
 logger = setup_logging()
+
+SEED = 42
+def set_seed(seed=SEED):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
